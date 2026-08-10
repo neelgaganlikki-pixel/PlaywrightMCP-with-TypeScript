2c92d0c7aca2e3653817f61984f7a8ea22556fb3
Neelgagan B R <your_github_email@example.com>
2026-08-10 13:55:00 +0530

    Checked for Flaky Test

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
       use: {
         browserName: 'chromium',
         ...devices['Desktop Chrome'],
-        headless: true,
+        headless: false,
 
         video: 'retain-on-failure',
         screenshot: 'only-on-failure',
