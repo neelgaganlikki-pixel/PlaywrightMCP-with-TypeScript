5f668a3be4cc418420bf1bed7622cb78b46bdb5b
Neelgagan B R <your_github_email@example.com>
2026-08-15 23:31:06 +0530

    added space in PW.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         screenshot: 'only-on-failure',
         trace: 'retain-on-failure',
         launchOptions: {
-          slowMo:1000,
+          slowMo:1000 ,
         },
       },
     },
