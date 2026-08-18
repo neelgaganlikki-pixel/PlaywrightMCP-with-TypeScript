431200e1b8d14a370dbad7f1ebfc150c5c87014d
Neelgagan B R <neelgaganlikki@gmail.com>
2026-08-18 19:56:02 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,39 +1,27 @@
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-  testDir: 'tests',
+    testDir: './tests',
 
-  timeout: 60000,
+    fullyParallel: false,
 
-  expect: {
-    timeout: 10000,
-  },
+    reporter: [
+        ['line'],
+        ['json', { outputFile: 'playwright-report.json' }]
+    ],
 
-  retries: 1, // Retry failed tests up to 2 times
-
-  workers: 1,
-  fullyParallel: false,
-
-  reporter: [
-    ['list'],
-    ['json', { outputFile: 'playwright-report.json' }]
-  ],
-
-  projects: [
-    {
-      name: 'chromium',
-      use: {
-        browserName: 'chromium',
-        ...devices['Desktop Chrome'],
-        headless: true,
-
-        video: 'retain-on-failure',
-        screenshot: 'only-on-failure',
-        trace: 'retain-on-failure',
-        launchOptions: {
-          slowMo: 1000,
-        },
-      },
+    use: {
+        headless: false,
+        slowMo: 1500,
+        screenshot: 'only-on-failure'
     },
-  ],
+
+    projects: [
+        {
+            name: 'chromium',
+            use: {
+                ...devices['Desktop Chrome']
+            }
+        }
+    ]
 });
